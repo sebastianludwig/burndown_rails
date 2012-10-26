b1ad790bac199e1b1576cd8e519a42f173f7afb3
Sebastian Ludwig <sebastian@lurado.de>
2012-10-26 22:00:33 +0200

    Initial commit (2h to set up a new rails project - not bad, not bad...)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Burndown::Application.config.secret_token = 'f0ccda1e7df1d40c067a8573a7bb3a115901277c10118a56bc0953b1d4ef664df412eca753c9d2f75909e5b5ef077ab495f6a5d11dc7016ad07fb6fc410f33be'
